6f563a7645927c0b5c269c7223b3387c9f883882
MykolaMyroshnychenko <80534437+MykolaMyroshnychenko@users.noreply.github.com>
2023-04-11 12:27:17 +0300

    Update CostDate.js

--- src/components/Costs/CostDate.js
+++ src/components/Costs/CostDate.js
@@ -3,7 +3,7 @@ import './CostDate.css';
 const CostDate = (props) => {
     const month = props.date.toLocaleString('ru-RU', {month: 'long'});
     const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('ru-RU', {day: '2-digit'}); //2-digit двойное число
+    const day = props.date.toLocaleString('ru-RU', {day: '2-digit'}); 
 
 return (
     <div className='cost-date'>
